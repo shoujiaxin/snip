34cc0a6cac7b9ee0b031eb4a41c599d2deb1ad34
Jiaxin Shou <shoujiaxin@gmail.com>
2021-11-27 21:53:30 +0800

    feat: add initial project

---
+++ Snip/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Snip
+//
+//  Created by Jiaxin Shou on 2021/11/27.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+}
