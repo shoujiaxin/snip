0ae5ca1b31f3aafd02f4fb7f931f2c534ce1aadf
Jiaxin Shou <shoujiaxin@gmail.com>
2022-01-12 23:48:43 +0800

    feat: make WindowInfo confirm to Codable

--- Snip/Models/WindowInfo.swift
+++ Snip/Models/WindowInfo.swift
@@ -7,7 +7,7 @@
 
 import Cocoa
 
-struct WindowInfo: Decodable {
+struct WindowInfo: Codable {
     /// The window’s alpha fade level. This number is in the range 0.0 to 1.0, where 0.0 is fully transparent and 1.0 is fully opaque.
     let alpha: Double
 
@@ -41,18 +41,31 @@ struct WindowInfo: Decodable {
     /// Specifies how the window device buffers drawing commands.
     let storeType: CGWindowBackingType
 
-    private enum CodingKeys: String, CodingKey {
-        case alpha = "kCGWindowAlpha"
-        case bounds = "kCGWindowBounds"
-        case isOnscreen = "kCGWindowIsOnscreen"
-        case layer = "kCGWindowLayer"
-        case memoryUsage = "kCGWindowMemoryUsage"
-        case name = "kCGWindowName"
-        case number = "kCGWindowNumber"
-        case ownerName = "kCGWindowOwnerName"
-        case ownerPID = "kCGWindowOwnerPID"
-        case sharingState = "kCGWindowSharingState"
-        case storeType = "kCGWindowStoreType"
+    private struct CodingKeys: CodingKey {
+        var intValue: Int?
+
+        var stringValue: String
+
+        init?(intValue: Int) {
+            self.intValue = intValue
+            stringValue = ""
+        }
+
+        init?(stringValue: String) {
+            self.stringValue = stringValue
+        }
+
+        static let alpha = Self(stringValue: kCGWindowAlpha as String)!
+        static let bounds = Self(stringValue: kCGWindowBounds as String)!
+        static let isOnscreen = Self(stringValue: kCGWindowIsOnscreen as String)!
+        static let layer = Self(stringValue: kCGWindowLayer as String)!
+        static let memoryUsage = Self(stringValue: kCGWindowMemoryUsage as String)!
+        static let name = Self(stringValue: kCGWindowName as String)!
+        static let number = Self(stringValue: kCGWindowNumber as String)!
+        static let ownerName = Self(stringValue: kCGWindowOwnerName as String)!
+        static let ownerPID = Self(stringValue: kCGWindowOwnerPID as String)!
+        static let sharingState = Self(stringValue: kCGWindowSharingState as String)!
+        static let storeType = Self(stringValue: kCGWindowStoreType as String)!
     }
 
     private enum BoundsCodingKeys: String, CodingKey {
@@ -63,22 +76,43 @@ struct WindowInfo: Decodable {
     }
 
     init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        alpha = try values.decode(Double.self, forKey: .alpha)
-        let boundsValue = try values.nestedContainer(keyedBy: BoundsCodingKeys.self, forKey: .bounds)
-        bounds = NSRect(x: try boundsValue.decode(Double.self, forKey: .x),
-                        y: try boundsValue.decode(Double.self, forKey: .y),
-                        width: try boundsValue.decode(Double.self, forKey: .width),
-                        height: try boundsValue.decode(Double.self, forKey: .height))
-        isOnscreen = try values.decode(Bool.self, forKey: .isOnscreen)
-        layer = try values.decode(Int.self, forKey: .layer)
-        memoryUsage = try values.decode(Int.self, forKey: .memoryUsage)
-        name = try values.decode(String.self, forKey: .name)
-        number = try values.decode(Int.self, forKey: .number)
-        ownerName = try values.decode(String.self, forKey: .ownerName)
-        ownerPID = try values.decode(Int.self, forKey: .ownerPID)
-        sharingState = .init(rawValue: try values.decode(UInt32.self, forKey: .sharingState)) ?? .none
-        storeType = .init(rawValue: try values.decode(UInt32.self, forKey: .storeType)) ?? .backingStoreRetained
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let boundsContainer = try container.nestedContainer(keyedBy: BoundsCodingKeys.self, forKey: .bounds)
+
+        alpha = try container.decode(Double.self, forKey: .alpha)
+        bounds = NSRect(x: try boundsContainer.decode(Double.self, forKey: .x),
+                        y: try boundsContainer.decode(Double.self, forKey: .y),
+                        width: try boundsContainer.decode(Double.self, forKey: .width),
+                        height: try boundsContainer.decode(Double.self, forKey: .height))
+        isOnscreen = try container.decode(Bool.self, forKey: .isOnscreen)
+        layer = try container.decode(Int.self, forKey: .layer)
+        memoryUsage = try container.decode(Int.self, forKey: .memoryUsage)
+        name = try container.decode(String.self, forKey: .name)
+        number = try container.decode(Int.self, forKey: .number)
+        ownerName = try container.decode(String.self, forKey: .ownerName)
+        ownerPID = try container.decode(Int.self, forKey: .ownerPID)
+        sharingState = .init(rawValue: try container.decode(UInt32.self, forKey: .sharingState)) ?? .none
+        storeType = .init(rawValue: try container.decode(UInt32.self, forKey: .storeType)) ?? .backingStoreRetained
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        var boundsContainer = container.nestedContainer(keyedBy: BoundsCodingKeys.self, forKey: .bounds)
+
+        try container.encode(alpha, forKey: .alpha)
+        try boundsContainer.encode(bounds.minX, forKey: .x)
+        try boundsContainer.encode(bounds.minY, forKey: .y)
+        try boundsContainer.encode(abs(bounds.width), forKey: .width)
+        try boundsContainer.encode(abs(bounds.height), forKey: .height)
+        try container.encode(isOnscreen, forKey: .isOnscreen)
+        try container.encode(layer, forKey: .layer)
+        try container.encode(memoryUsage, forKey: .memoryUsage)
+        try container.encode(name, forKey: .name)
+        try container.encode(number, forKey: .number)
+        try container.encode(ownerName, forKey: .ownerName)
+        try container.encode(ownerPID, forKey: .ownerPID)
+        try container.encode(sharingState.rawValue, forKey: .sharingState)
+        try container.encode(storeType.rawValue, forKey: .storeType)
     }
 }
 
